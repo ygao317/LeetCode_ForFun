b7dea4051f66b25fe1a027e02378a84606fd6a13
ygao317 <gaoyuan.c2s@gmail.com>
2022-11-14 20:30:44 +0800

    Create LeetCode_2469.cpp
    Too Easy

---
+++ WeekendContest/LeetCode_2469.cpp
@@ -0,0 +1,8 @@
+// 2469. Convert the Temperature
+
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        return { celsius+273.15, celsius*1.8 + 32 };
+    }
+};
