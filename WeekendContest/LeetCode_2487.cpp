76f5396a0131ea8a62928efd0c13a175547608a9
ygao317 <gaoyuan.c2s@gmail.com>
2022-11-30 15:19:03 +0800

    Create LeetCode_2487.cpp
    Recursive version... I am using stack to solve it during the contest.

---
+++ WeekendContest/LeetCode_2487.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        if (!head) return NULL;
+        head->next = removeNodes(head->next);
+        return head->next && head->val < head->next->val ?  head->next : head;
+    }
+};
