02ffb6df79d739dc52918062143e5ad16e19fdcf
ygao317 <gaoyuan.c2s@gmail.com>
2022-12-05 09:42:07 +0800

    Create LeetCode_876.cpp
    Easy two pointer question

---
+++ DailyChallenges/CPP/LeetCode_876.cpp
@@ -0,0 +1,11 @@
+// 876. Middle of the Linked List @ 2022/12/05
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next)
+            slow = slow->next, fast = fast->next->next;
+        return slow;
+    }
+};
