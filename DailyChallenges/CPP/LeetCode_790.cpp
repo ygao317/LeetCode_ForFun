abe03e2ddd0b2954b18c2cc4bef2060b163822b3
ygao317 <gaoyuan.c2s@gmail.com>
2022-12-24 14:47:58 +0800

    Create LeetCode_790.cpp
    Not sure if it is math problem or DP problem...

---
+++ DailyChallenges/CPP/LeetCode_790.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numTilings(int N) {
+        vector<long long> v(1001,0);
+        v[1]=1, v[2]=2, v[3]=5;
+        if(N<=3)
+            return v[N];
+        for(int i=4;i<=N;++i)
+            v[i]=(2*v[i-1]+v[i-3]) % int(1e9+7); 
+        return v[N];      
+    }
+};
