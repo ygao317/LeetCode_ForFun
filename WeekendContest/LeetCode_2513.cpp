063e63222b65ea58b4fb580c89ed8d26719f36a5
ygao317 <gaoyuan.c2s@gmail.com>
2023-01-06 21:05:13 +0800

    Create LeetCode_2513.cpp
    It is a math problem. Can simulate though.

---
+++ WeekendContest/LeetCode_2513.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int getMax(int cnt, long long d1, long long d2 = 1) {
+        long long d = lcm(d1, d2);
+        return cnt + cnt / (d - 1) - (cnt % (d - 1) ? 0 : 1);
+    }
+    int minimizeSet(int d1, int d2, int cnt1, int cnt2) {
+        return max({getMax(cnt1, d1), getMax(cnt2, d2), getMax(cnt1 + cnt2, d1, d2) });
+    }
+};
