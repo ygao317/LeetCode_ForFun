4cb876ac7f835f39b28760893cd53941a4869248
ygao317 <gaoyuan.c2s@gmail.com>
2023-02-06 20:40:12 +0800

    Create LeetCode_1470.cpp

---
+++ DailyChallenges/CPP/LeetCode_1470.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(2*n);
+        int j = 0;
+        for (int i = 0; i<n; ++i){
+            ans[j++] = nums[i];
+            ans[j++] = nums[i+n];
+        }
+        return ans;
+    }
+};
