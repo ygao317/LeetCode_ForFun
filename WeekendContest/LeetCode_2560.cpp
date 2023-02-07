21ed324e1fb9725c275a7f49d06eb30234509084
ygao317 <gaoyuan.c2s@gmail.com>
2023-02-07 08:46:58 +0800

    Update LeetCode_2560.cpp

--- WeekendContest/LeetCode_2560.cpp
+++ WeekendContest/LeetCode_2560.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int minCapability(vector<int>& nums, int k) {
-        vector<vector<int>> dp(2, vector<int>(k+1,-1)); // init as -1
-        dp[0][0] = 0;
-        
-        for (int i = 0; i < nums.size(); ++i){
-            int num = nums[i];
-            // if take it
-            for (int j=min(k-1, i/2); j>=0; --j){
-                dp[0][j+1] = max(dp[0][j], num);
-            }
-            swap(dp[0], dp[1]);
+    int minCapability(vector<int>& A, int k) {
+        int left = 1, right = 1e9, n = A.size();
+        while (left < right) {
+            int mid = (left + right) / 2, take = 0;
+            for (int i = 0; i < n; ++i)
+                if (A[i] <= mid) {
+                    take += 1;
+                    i++;
+                }
+            if (take >= k)
+                right = mid;
+            else
+                left = mid + 1;
         }
-        
-        return min(dp[0][k], dp[1][k]);
+        return left; //left == right
     }
 };
