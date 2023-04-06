6c871f185e8dfa7f5c2c66bf2f8cbbb52e6af4bb
ygao317 <gaoyuan.c2s@gmail.com>
2023-04-06 19:43:40 +0800

    Create LeetCode_1254.scala
    DFS

---
+++ DailyChallenges/Scala/LeetCode_1254.scala
@@ -0,0 +1,19 @@
+object Solution {
+    def closedIsland(grid: Array[Array[Int]]): Int = {
+        val m = grid.length; val n = grid(0).length
+        def fill(i: Int, j: Int): Int = if (i<0 || j<0 || i>=m || j>=n || grid(i)(j) == 1) 0 else{
+            grid(i)(j) = 1
+            1 + fill(i+1, j) + fill(i-1,j) + fill(i, j+1) + fill(i, j-1)
+        }
+
+        for (i <- List(0, m-1); j <- 0 until n) fill(i,j)
+        for (j <- List(0, n-1); i <- 0 until m) fill(i,j)
+
+        var ans = 0
+        for (i <- 0 until m; j <- 0 until n)
+            if (fill(i,j) > 0) 
+                ans+=1
+        
+        ans
+    }
+}
