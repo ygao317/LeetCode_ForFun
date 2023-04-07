aac053ad3f60bbcc1139d383b7cfffe2554b2e8c
ygao317 <gaoyuan.c2s@gmail.com>
2023-04-07 08:16:57 +0800

    Create LeetCode_1020.cpp
    Fill the boundary first.

---
+++ DailyChallenges/CPP/LeetCode_1020.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        function <int(int,int)> fill = [&](int i, int j){
+            if (i < 0 || j < 0 || i >= m || j >= n || !grid[i][j])
+                return 0;
+            return !(grid[i][j] = 0) + fill(i + 1, j) + fill(i, j + 1) 
+        + fill(i - 1, j) + fill(i, j - 1);
+        };
+
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (i * j == 0 || i == m - 1 || j == n - 1)
+                    fill(i, j);
+        
+        int ans = 0;
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                ans += fill(i,j);
+        return ans;
+    }
+};
