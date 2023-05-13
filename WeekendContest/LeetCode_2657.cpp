2fda9eed582bec890cc28c71d63c633b01f5f7e5
ygao317 <gaoyuan.c2s@gmail.com>
2023-05-13 18:12:24 +0800

    Create LeetCode_2657.cpp
    Nice one to use set property

---
+++ WeekendContest/LeetCode_2657.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        unordered_set<int> s;
+        vector<int> ans;
+        for (int i=0; i<A.size(); ++i){
+            s.insert(A[i]);
+            s.insert(B[i]);
+            ans.push_back(2*(i+1) - s.size());
+        }
+        return ans;
+    }
+};
