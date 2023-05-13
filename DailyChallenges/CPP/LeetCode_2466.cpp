f9eb20a01c09000f2b7c33be83d1e54b50ed363f
ygao317 <gaoyuan.c2s@gmail.com>
2023-05-13 09:22:40 +0800

    Create LeetCode_2466.cpp
    DP

---
+++ DailyChallenges/CPP/LeetCode_2466.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        vector<long> v(high+1, 0L); v[zero] += 1; v[one] += 1;
+        long ans=0, mod = 1e9+7;
+        for (int i=0; i<high+1; ++i){
+            v[i] = (v[i] + ((i-zero > 0)? v[i-zero]: 0) + ((i-one > 0)? v[i-one]: 0)) % mod;
+            if (i>= low)
+                ans = (ans + v[i]) % mod;
+        }
+        return ans;
+    }
+};
