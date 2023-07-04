fb66ca85ca577d489907fb5a17da94db919a1f1f
ygao317 <gaoyuan.c2s@gmail.com>
2023-07-04 20:48:42 +0800

    Create LeetCode_2740.cpp
    sorting

---
+++ WeekendContest/LeetCode_2740.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findValueOfPartition(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int ans = nums[n-1] - nums[0];
+        for (int i = 1; i< n; ++i){
+            ans = min(ans, nums[i]-nums[i-1]);
+            if (ans == 0) return 0;
+        }
+        return ans;
+    }
+};
