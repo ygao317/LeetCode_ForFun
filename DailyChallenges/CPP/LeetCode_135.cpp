4b8add2a622fe298f1be1ae3dad821881eddee7f
ygao317 <gaoyuan.c2s@gmail.com>
2023-09-13 08:27:11 +0800

    Create LeetCode_135.cpp
    Two round of loop

---
+++ DailyChallenges/CPP/LeetCode_135.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> cs(ratings.size(), 1);
+        for (int i = 1; i<n; ++i)
+            if (ratings[i] > ratings[i-1]) cs[i] = cs[i-1]+1;
+        for (int i = n-2; i>=0; --i)
+            if (ratings[i] > ratings[i+1]) cs[i] = max(cs[i], cs[i+1]+1);
+        
+        return accumulate(cs.begin(), cs.end(), 0);
+    }
+};
